3e80053a49b771e08c85893875eff6dff6c2d819
Chloe Nott <chloe@nott.email>
2021-09-27 16:53:54 -0700

    Click counting added. Not yet debugged.

--- script.js
+++ script.js
@@ -36,34 +36,62 @@ function displayImage(productObject) {
     imageElement.src = productObject.path;
     imageElement.id = productObject.name;
     document.getElementById("image-group").appendChild(imageElement);
+
+    // Keep track of how many times an image has been displayed.
+    // Note that user may not click any image; the display count will still increment.
+    productObject.countDisplayed = 1 + productObject.countDisplayed;
 }
 
-// Display n random images
-function displayNRandomImages(numberOfImagesToShow) {
+// Display new image set
+function displayNRandomImages() {
     let alreadyDisplayed = [];
     
-    for (let i = 0; i < numberOfImagesToShow; i++) {
+    // Only show new image set if user has comparisons left to be asked.
+    if (comparisonsAsked < comparisonsToAsk) {
 
-        // Pick a random index to show. Prevent duplicates.
+        comparisonsAsked++;
         let randomIndex;
-        do {
-            randomIndex = Math.floor(Math.random() * Product.productList.length); 
-        } while (alreadyDisplayed.includes(randomIndex));
 
-        // Keep track of which indices (image objects) have already been selected.
-        alreadyDisplayed.push(randomIndex);
+        // Remove existing images
+        document.getElementById("image-group").innerHTML = '';
 
-        // Show the product at the random index.
-        displayImage(Product.productList[randomIndex]);
+        // Display n random images
+        for (let i = 0; i < numberOfImagesToShow; i++) {
+
+            // Pick a random index to show. Prevent duplicates.
+            do {
+                randomIndex = Math.floor(Math.random() * Product.productList.length); 
+            } while (alreadyDisplayed.includes(randomIndex));
+
+            // Keep track of which indices (image objects) have already been selected.
+            alreadyDisplayed.push(randomIndex);
+
+            // Show the product at the random index.
+            displayImage(Product.productList[randomIndex]);
+        }
     }
+
 }
 
 
-// Function to detect click and increment values. Calls displayNRandomImages when complete.
+// Function to log click and increment values.
+// Refresh images after logging.
+function logClick(event) {
 
+    // Get product object from the selected image's id and increment the number of times it's been clicked.
+    for (let i = 0; Product.productList.length; i++) {
+        if (event.target.id == Product.productList[i].name) {
+            Product.productList[i].countClicked = 1 + Product.productList[i].countClicked
+            displayNRandomImages(); // Show next image set.
+            break;
+        }
+    }
+
+}
 
 // Set how many times user is asked to make a comparison
 const comparisonsToAsk = 25;
+let comparisonsAsked = 0;
 const numberOfImagesToShow = 3;
 
 // Create a product object for each image file.
@@ -71,8 +99,12 @@ const numberOfImagesToShow = 3;
 for (let file of files) {
     new Product(file);
 }
+
+// Display n random images upon page load.
 displayNRandomImages(numberOfImagesToShow);
-//displayImage(Product.productList[0])
+
+// Log which image has been clicked and refresh images shown.
+document.getElementById("image-group").addEventListener("click", logClick)
 
 
 
